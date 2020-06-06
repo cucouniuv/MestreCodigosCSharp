06361d1f9ee636b894b8aa6e899847076c5b586a
Luis <luis.olivetti@db1.com.br>
2020-06-06 20:48:27 -0300

    Questões 7 e 8

---
+++ Questao7/Questao7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Questao7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faça uma aplicação ler 4 números inteiros e calcular a soma dos que forem pares.");
+            Console.WriteLine("Digite o primeiro valor:");
+            int v1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int v2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro valor:");
+            int v3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o quarto valor:");
+            int v4 = int.Parse(Console.ReadLine());
+
+            List<int> lista = new List<int> { v1, v2, v3, v4};
+
+            int soma = 0;
+            foreach (int x in lista)
+            {
+                if ((x % 2) == 0)
+                    soma += x;
+            }
+
+            Console.WriteLine("Valor da soma: {0}", soma.ToString());
+        }
+    }
+}
---
+++ Questao8/Questao8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Questao8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faça uma aplicação ler N valores decimais, imprima os valores em ordem crescente e decrescente.");
+
+            List<decimal> lista = new List<decimal>();
+
+            while (true)
+            {
+                Console.WriteLine("Digite um valor decimal ou digite Parar:");
+                string valor = Console.ReadLine();
+                if (valor == "Parar")
+                {
+                    break;
+                }
+
+                bool teste = decimal.TryParse(valor, out decimal valordecimal);
+
+                if (!(teste))
+                {
+                    Console.WriteLine("Valor decimal inválido.");
+                }
+
+                lista.Add(valordecimal);
+            }
+
+            lista.Sort();
+
+            int i;
+
+            Console.WriteLine("Valores em ordem crescente:");
+            for (i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+
+            Console.WriteLine("Valores em ordem decrescente:");
+            for (i = (lista.Count - 1); i >= 0; i--)
+            {
+                Console.WriteLine(lista[i]);
+            }
+        }
+    }
+}
