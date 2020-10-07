42cf5f108f2de7a33627c81e0f1543a7e7d99b22
Luis <luis.olivetti@db1.com.br>
2020-10-07 19:22:44 -0300

    Correção item 3 POO
    3. régua baixa: Conta corrente está permitindo ficar com saldo negativo, está permitindo sacar um valor maior que o saldo.
       régua alta: A declaração de propriedade pode ser simplicada sem precisar declarar atributos privados.
       Wow: Usando AAA nos testes.

--- Questao3POO/Questao3POO/ContaCorrente.cs
+++ Questao3POO/Questao3POO/ContaCorrente.cs
@@ -1,23 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Questao3POO
 {
     public class ContaCorrente : ContaBancaria, IImprimivel
     {
-        private double _taxaOperacao;
-
-        public double TaxaOperacao
-        {
-            get { return _taxaOperacao; }
-            set { _taxaOperacao = value; }
-        }
+        public double TaxaOperacao { get; set; }
 
         public override void Depositar(double valor)
         {
-            Saldo += valor - _taxaOperacao;
-            //throw new NotImplementedException();
+            Saldo += valor - TaxaOperacao;
         }
 
         public string MostrarDados()
@@ -30,8 +21,10 @@ namespace Questao3POO
 
         public override void Sacar(double valor)
         {
-            Saldo -= valor - _taxaOperacao;
-            //throw new NotImplementedException();
+            if ((Saldo - valor) < 0)
+                throw new Exception("Conta " + NumeroConta + " possui saldo insuficiente para realizar este saque.");
+
+            Saldo -= valor - TaxaOperacao;
         }
     }
 }
--- Questao3POO/Questao3POO/ContaEspecial.cs
+++ Questao3POO/Questao3POO/ContaEspecial.cs
@@ -1,18 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Questao3POO
 {
     public class ContaEspecial : ContaBancaria, IImprimivel
     {
-        private double _limite;
-
-        public double Limite
-        {
-            get { return _limite; }
-            set { _limite = value; }
-        }
+        public double Limite { get; set; }
 
         public override void Depositar(double valor)
         {
@@ -29,7 +21,7 @@ namespace Questao3POO
 
         public override void Sacar(double valor)
         {
-            if ((Saldo - valor) < (- _limite))
+            if ((Saldo - valor) < (- Limite))
                 throw new Exception("Conta " + NumeroConta + " possui saldo insuficiente para realizar este saque.");
 
             Saldo -= valor;
